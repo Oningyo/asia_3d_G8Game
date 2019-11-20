876f39e91c78d7ff1b586a588697f99a9f6e0644
Oningyo <55180582+Oningyo@users.noreply.github.com>
2019-11-20 16:16:20 +0800

    2019/11/20
    學習參數指令

--- 3D Unity Project/Assets/Script/G8Player.cs
+++ 3D Unity Project/Assets/Script/G8Player.cs
@@ -2,8 +2,59 @@
 
 public class G8Player : MonoBehaviour
 {
-    int speed = 10;
-    float trun = 20.5f;
-    bool mission = false;
-    string Name="G8雞";
+    [Header("G8雞的控制"), Range(1, 100)]
+    public int speed = 10;
+    [Tooltip("選轉速度"), Range(1, 100)]
+    public float trun = 20.5f;
+    public bool mission = false;
+    public string Name = "G8雞";
+
+    public void action(int speed)
+    {
+        print("雞雞速度" + speed);
+
+    }
+
+    public void turn_round(float trun)
+    {
+
+
+        print("雞雞旋轉" + trun);
+
+
+    }
+
+    public void scream(string scream="叫")
+    {
+
+
+        print("雞雞叫" + scream);
+
+
+    }
+    public void pick (bool pick = true)
+    {
+
+
+        print("雞雞撿東西" + pick);
+
+
+    }
+    private void Target (bool complete =true)
+    {
+
+
+        print("任務成功" +complete );
+
+
+    }
+
+
+
 }
+
+
+
+
+
+
---
+++ 3D Unity Project/Assets/Script/Learnmethod.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Learnmethod : MonoBehaviour
+{
+
+
+
+
+    private void Drive(int speed,float count,string prop ="無",string direction="前方" )
+    {
+
+        print("開車，時數"+ speed);
+        print("開車加速" + count);
+        print("屬性補給包" + prop);
+        print("方向" + direction);
+    }
+    private void Start()
+    {
+
+        print("哈喽你好嗎");
+        Drive(100,10.5f,"火屬性");
+        Drive(200,50.8f,"冰");
+        Drive(300,700.8f,"水");
+        Drive(400, 500.8f, direction : "前後方");
+    }
+
+
+
+}
