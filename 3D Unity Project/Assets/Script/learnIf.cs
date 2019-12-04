105169e16a1c7f4523ba2fe36811e2218532041b
Oningyo <55180582+Oningyo@users.noreply.github.com>
2019-12-04 16:01:50 +0800

    2019_12_04
    學習數學運算子、判斷式

---
+++ 3D Unity Project/Assets/Script/learnIf.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class learnIf : MonoBehaviour
+{
+    public bool opendoor;
+
+    public int score = 100;
+
+    public int combo;
+
+    private void Start()
+    {
+        //規則:()為true才執行
+        if (true)
+        {
+
+            print("測試!");
+
+        }
+
+    }
+
+   private void Update()
+    {
+        ///()為true執行if,反之()為false執行else
+        if(opendoor)
+        {
+            print("開門");
+        }
+        else 
+        {
+            print("關門");
+        
+        
+        }
+        if (score >= 60)
+        {
+            print("及格!");
+
+        }
+        else if(score<=60)
+        {
+            print("該補考啦!");
+
+
+        }
+        else if (score >= 40) 
+        {
+
+            print("課後輔導!");
+
+        }
+       else 
+        {
+
+            print("你重修吧");
+        
+        
+        }
+        if (combo >=150)
+        {
+            print("攻擊10倍!");
+        }
+        else if(combo>=100)
+        {
+
+            print("攻擊五倍");
+        }
+        else if(combo>=50)
+        {
+            print("攻擊2倍!");
+
+        }
+
+
+
+    }
+
+
+}
---
+++ 3D Unity Project/Assets/Script/learnOparator.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class learnOparator : MonoBehaviour
+{
+    public int A = 10, B = 3;
+    public int C = 1;
+    public int D = 1, E = 1;
+    public int F = 10;
+    public float G = 1.5F, H = 0.9F;
+    public float hp = 50;
+    public int enemy;
+    public bool key;
+
+
+    private void Start()
+    {
+#region 數學運算子
+        print(A + B);
+        print(A - B);
+        print(A * B);
+        print(A / B);
+        print(A % B);
+
+
+        C = 99 + 1;///指派是先運算右邊在左邊
+        print(C);
+
+       C = C + 1;///原始寫法
+        print(D++);///後置遞增
+        print(++E);///前置遞增
+
+
+        // 加法指派 - * / %
+        // F = F + 100;
+        F += 100;
+        print(F);
+#endregion
+
+#region 比較運算子
+        print(G > H);
+        print(G < H);
+        print(G >= H);
+        print(G <= H);
+        print(G == H);
+        print(G != H);
+ #endregion
+
+ # region 邏輯運算子
+        //&&(並且)兩者皆有則為true,否則為false
+        print(true && true);
+        print(false && true);
+        print(false && false);
+        print(true && false);
+
+        //||(或者)有true必定結果為true
+        print(true || true);
+        print(false || true);
+        print(true || false);
+        print(false || false);
+
+        //!顛倒運算子
+        print(!true);
+        print(!false);
+#endregion
+
+
+    }
+
+    private void Update()
+    {
+
+        print("死亡" + (hp <= 0));
+
+        print("過關" + (key || enemy >= 5));
+
+
+
+
+
+    }
+
+
+
+}
